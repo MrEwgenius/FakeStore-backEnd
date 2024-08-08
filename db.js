7a9d046fb21df6779c143ea4be33b5dd9f170d0a
Bolynskii Ewgenii <119881162+MrEwgenius@users.noreply.github.com>
2024-08-08 21:46:31 +0300

    pg

--- db.js
+++ db.js
@@ -1,16 +1,15 @@
-const { Sequelize } = require('sequelize')
+const { Sequelize } = require("sequelize");
 
 module.exports = new Sequelize(
     process.env.DB_NAME, // Название БД
     process.env.DB_USER, // Пользователь
     process.env.DB_PASSWORD, // ПАРОЛЬ
-    
-    
+
     {
-        dialect: 'postgres',
+        dialect: "postgres",
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
-        sync: true
-
+        sync: true,
+        dialectModule: "pg",
     }
-) 
+);
