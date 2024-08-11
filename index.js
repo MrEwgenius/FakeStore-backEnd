1ea58e6b44c00dc8fbcef745e3f719de9955a59d
Bolynskii Ewgenii <119881162+MrEwgenius@users.noreply.github.com>
2024-08-11 20:16:23 +0300

    ed

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
 const express = require('express')
-const sequelize = require('../db')
-const models = require('../models/models')
+const sequelize = require('./db')
+const models = require('./models/models')
 const cors = require('cors')
 const fileUpload = require('express-fileupload')
-const router = require('../routes/index')
-const errorHandler = require('../middleware/ErrorHandlingMiddleware')
+const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 const path = require('path')
 
 const PORT = process.env.PORT || 3030
